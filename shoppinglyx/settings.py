b37cdcc1c9885b9ee4880894de1dcf0a6cd8788a
DIkshant Boora <62752878+dikshant7@users.noreply.github.com>
2023-02-03 20:04:05 +0530

    Update settings.py

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -135,3 +135,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
